dc8615030bb674e34946822288ae465db779c59d
Luca Cilfone <info@fareilweb.com>
2019-03-30 09:40:56 +0100

    upd

--- make-cert.bat
+++ make-cert.bat
@@ -2,9 +2,9 @@
 set /p domain="Enter Domain: "
 set OPENSSL_CONF=../../conf/openssl.cnf
 
-if not exist .\generated-certs\%domain% mkdir .\generated-certs\%domain%
+if not exist ..\%domain% mkdir ..\%domain%
 
-.\openssl req -config make-cert.conf -new -sha256 -newkey rsa:2048 -nodes -keyout .\generated-certs\%domain%\server.key -x509 -days 365 -out .\generated-certs\%domain%\server.crt
+.\openssl req -config make-cert.conf -new -sha256 -newkey rsa:2048 -nodes -keyout ..\%domain%\server.key -x509 -days 365 -out ..\%domain%\server.crt
 
 echo.
 echo -----
